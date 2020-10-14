d84c12aa806bc606379a08407a683d94e78e41d8
Roee Angel <roee.angel93@gmail.com>
2020-10-14 23:07:19 +0300

    add function to get data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React , {useState, useEffect} from "react";
+import axios from 'axios'
 import "./App.css";
 function App() {
   const [pokedex,setPokedex] = useState([])
-  const [pokemon, setPokemon] = useState({})
+  const [wildPokemon, setWildPokemon] = useState({});
   useEffect(()=>{
-
+    encounterWildPokemon()
   },[])
+  const pokeId = () => {
+    const min = Math.ceil(1)
+    const max = Math.floor(151)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+  const encounterWildPokemon = () => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/' + pokeId())
+      .then(response => {
+        setWildPokemon(response.data);
+      })
+  }
   return (
     <div className="app-wrapper">
 
