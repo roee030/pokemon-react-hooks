e4015a4eef262be89924310a0817bee4e441f34e
Roee Angel <roee.angel93@gmail.com>
2020-10-14 23:48:28 +0300

    add background and more function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React , {useState, useEffect} from "react";
 import axios from 'axios'
+import pokedexImg from './pokedexImg.jpg'
 import "./App.css";
 function App() {
+ 
   const [pokedex,setPokedex] = useState([])
+  const [pokemonId,setPokemonId] = useState()
   const [wildPokemon, setWildPokemon] = useState({});
   useEffect(()=>{
     encounterWildPokemon()
-  },[])
+    pokeId()
+  },[pokemonId])
   const pokeId = () => {
     const min = Math.ceil(1)
     const max = Math.floor(151)
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    setPokemonId( Math.floor(Math.random() * (max - min + 1)) + min)
+    return (Math.floor(Math.random() * (max - min + 1)) + min)
   }
   const encounterWildPokemon = () => {
     axios
@@ -21,6 +26,15 @@ function App() {
   }
   return (
     <div className="app-wrapper">
+     
+
+     
+        <img  src={pokedexImg} class=" outer-image " ></img>
+        <div class="inner-image">
+
+        <img  src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + wildPokemon.id + ".png"} className="sprite" />
+        <button onClick={()=>pokeId()}>{wildPokemon.name}</button>
+        </div>
 
     </div>
   );
